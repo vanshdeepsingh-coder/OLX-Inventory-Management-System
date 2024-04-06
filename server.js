2150c612e686a0e214ecd58fd05395d6719894c6
OLX <olx@Admins-MacBook-Pro.local>
2024-04-07 04:53:40 +0530

    created a server for the application

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const server = express();
+const PORT=3000;
+
+server.listen(PORT,(error)=>{
+    if(!error){
+        console.log(`Server is running successfully on port number: ${PORT}`);
+    }
+    else{
+        console.log(`An error occured while starting the server`);
+        console.log(`Error Code: ${error}`);
+    }
+})
+
+module.exports=server;
