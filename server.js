023e1f45660ba9b2355a7fda0eaf4b1047e61f74
Vanshdeep Singh <vanshdeepsingh549@gmail.com>
2024-04-11 12:29:49 +0530

    making the server class and setting up the port

--- server.js
+++ server.js
@@ -2,3 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.PORT = process.env.PORT || 3000;
+
+        this.connectToDB();
+
+        this.app.use(bodyParser.json());
+
+        this.app.use(routes);
+
+        this.startServer();
+    }
