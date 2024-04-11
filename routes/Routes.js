1c99531aa3f35545d437a215b5c36023d1acb25c
Vanshdeep Singh <vanshdeepsingh549@gmail.com>
2024-04-11 14:01:56 +0530

    created class version of router

--- routes/Routes.js
+++ routes/Routes.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const InventoryController = require('../controller/inventoryController');
 
-const router = express.Router();
-const inventoryController = new InventoryController()
+class Routes{
 
-router.post('/inventory', inventoryController.createInventory.bind(inventoryController));
-router.put('/inventory/:sku', inventoryController.updateInventory.bind(inventoryController));
-router.delete('/inventory/:sku', inventoryController.deleteInventory.bind(inventoryController));
-router.get('/',inventoryController.createInventory.bind(inventoryController));
+    initializeRouter(){
+        const router=express.Router();
 
-module.exports = router;
+        const inventoryController = new InventoryController()
+
+        router.post('/inventory', inventoryController.createInventory.bind(inventoryController));
+        router.put('/inventory/:sku', inventoryController.updateInventory.bind(inventoryController));
+        router.delete('/inventory/:sku', inventoryController.deleteInventory.bind(inventoryController));
+        router.get('/',inventoryController.createInventory.bind(inventoryController));
+
+        return router;
+    }
+}
+
+const routes=new Routes();
+module.exports = routes;
