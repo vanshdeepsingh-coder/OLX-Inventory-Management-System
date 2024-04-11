c6d373648ac3072f76bbdb6395b0ae8cddd6a120
Vanshdeep Singh <vanshdeepsingh549@gmail.com>
2024-04-11 12:13:40 +0530

    creating the rest api's for the router

--- routes/Routes.js
+++ routes/Routes.js
@@ -3,3 +3,10 @@ const InventoryController = require('./inventoryController');
 
 const router = express.Router();
 const inventoryController = new InventoryController()
+
+outer.post('/inventory', inventoryController.createInventory.bind(inventoryController));
+router.put('/inventory/:sku', inventoryController.updateInventory.bind(inventoryController));
+router.delete('/inventory/:sku', inventoryController.deleteInventory.bind(inventoryController));
+ router.get('/',inventoryController.createInventory.bind(inventoryController));
+
+module.exports = router;
