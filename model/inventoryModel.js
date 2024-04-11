30bff1bb39a0ee4bea1a48ccff6448b8adc24d22
Vanshdeep Singh <vanshdeepsingh549@gmail.com>
2024-04-11 12:18:20 +0530

    creating the get Schema function for inventory model

--- model/inventoryModel.js
+++ model/inventoryModel.js
@@ -4,3 +4,29 @@ class Inventory {
     constructor() {
         this.InventoryModel = mongoose.model('Inventory', this.getSchema());
     }
+
+    getSchema() {
+        return new mongoose.Schema({
+            sku: { type: String, required: true },
+            type: String,
+            status: { type: String, enum: ['CREATED', 'PROCURED', 'SOLD'], default: 'CREATED' },
+            location: String,
+            attributes: {
+                vin: String,
+                make: String,
+                model: String,
+                trim: String,
+                year: Number
+            },
+            pricing: {
+                cost: Number,
+                sellingPrice: Number
+            },
+            metadata: {
+                createdAt: { type: Date, default: Date.now },
+                lastUpdatedAt: { type: Date, default: Date.now },
+                createdBy: String,
+                lastUpdatedBy: String
+            }
+        });
+    }
