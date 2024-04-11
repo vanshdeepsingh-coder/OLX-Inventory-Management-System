11374ae626367ab1fd1339a3cd6b6ea131ed3411
Vanshdeep Singh <vanshdeepsingh549@gmail.com>
2024-04-11 12:28:33 +0530

    creating the server

--- server.js
+++ server.js
@@ -1,31 +1,4 @@
-const express=require('express');
-const updateInventory=require('./updateInventory');
-const createInventory=require('./createInventory');
-const fs=require('fs');
-const server=express();
-const PORT=3000;
-
-server.listen(PORT,(error)=>{
-    if(!error){
-        console.log(`Server is running successfully on port number: ${PORT}`);
-    }
-    else{
-        console.log(`An error occured while starting the server`);
-        console.log(`Error Code: ${error}`);
-    }
-})
-
-function readJSONFileSync(filepath){
-    var file=fs.readFileSync(filepath);
-    return JSON.parse(file)
-}
-
-createInventory('data.json');
-
-server.get('/',(req,res)=>{
-    res.send(readJSONFileSync('data.json'));
-})
-
-// updateInventory('data.json');
-
-module.exports=server;
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
