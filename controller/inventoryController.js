0735b46062b547afbe22448d2da28887ef152cfc
Vanshdeep Singh <vanshdeepsingh549@gmail.com>
2024-04-11 12:05:49 +0530

    creating the update inventory function

--- controller/inventoryController.js
+++ controller/inventoryController.js
@@ -16,3 +16,15 @@ class InventoryController {
             res.status(400).json({ message: err.message });
         }
     }
+
+    async updateInventory(req, res) {
+        try {
+            const InventoryModel = this.inventoryModel.getModel();
+            const sku = req.params.sku;
+            const inventoryData = req.body;
+            const updatedInventory = await InventoryModel.findOneAndUpdate({ sku }, inventoryData, { new: true });
+            res.json(updatedInventory);
+        } catch (err) {
+            res.status(400).json({ message: err.message });
+        }
+    }
