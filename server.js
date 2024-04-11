3dfbf7b733443552bd7ba09a68ffa8160281da2f
Vanshdeep Singh <vanshdeepsingh549@gmail.com>
2024-04-11 12:30:41 +0530

    adding the mongodb server logic

--- server.js
+++ server.js
@@ -16,3 +16,13 @@ class Server {
 
         this.startServer();
     }
+
+    connectToDB() {
+        mongoose.connect('mongodb+srv://anmolsankadiya:anmol@cluster0.y6u1lez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+        , {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        .then(() => console.log('MongoDB Connected'))
+        .catch(err => console.log(err));
+    }
