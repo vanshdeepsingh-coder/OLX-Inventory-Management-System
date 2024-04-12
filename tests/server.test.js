5a9fea8ba9512436d0235ec3a929c80deb7e41d7
Vanshdeep Singh <vanshdeepsingh549@gmail.com>
2024-04-12 12:20:16 +0530

    adding the tests for mongoDB

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,3 +1,9 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('./server');
+
+describe('Server',() => {
+    it('should connect to MongoDB', (done) =>{
+        expect(server.connectToDB()).to.eventually.be.fulfilled.and.notify(done);
+    })
+})
