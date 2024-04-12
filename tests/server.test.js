7a6205ee944646bbd0d9d752d302fbc084d8659c
Vanshdeep Singh <vanshdeepsingh549@gmail.com>
2024-04-12 12:27:14 +0530

    adding the server test logic

--- tests/server.test.js
+++ tests/server.test.js
@@ -6,4 +6,13 @@ describe('Server',() => {
     it('should connect to MongoDB', (done) =>{
         expect(server.connectToDB()).to.eventually.be.fulfilled.and.notify(done);
     })
+
+    it('should start the server', (done) => {
+        chai.request(server.app)
+        .get('/') 
+        .end((err, res) => {
+            expect(res).to.have.status(200);
+            done();
+        });
+    });
 })
